69eae47e2b11ea446a106f4cac798b2745bf3424
Alejandro <50674421+ajserrano9310@users.noreply.github.com>
2022-07-24 16:44:45 -0600

    full fetch now works

--- weather-project/logic.js
+++ weather-project/logic.js
@@ -1,6 +1,8 @@
 
 var uri = "https://api.openweathermap.org/data/2.5/weather?lat=40.7608&lon=-111.8910&units=metric&appid=117dc4bba31cb1ddc1ceeb399015666e"
 
+const API_KEY = "117dc4bba31cb1ddc1ceeb399015666e";
+
 const getInformation = function(){
 
     fetch(uri)
@@ -42,4 +44,40 @@ function processWeatherResponse(obj){
     let weatherText = document.getElementsByClassName("temp-wrapper")[0];
     weatherText.innerHTML = obj.temperature;
 
+}
+
+function processUserRequest(){
+
+    let city = document.getElementById("cityName").value; 
+    let cityUri = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=5&appid="+API_KEY;
+
+    fetch(cityUri)
+    .then(function(response){
+        return response.json();
+
+    }).then(function(responseJson){
+
+        const {lat, lon} = responseJson[0];
+        console.log(responseJson);
+
+        let weatherURI = "https://api.openweathermap.org/data/2.5/weather?lat=" +lat + "&" + "lon=" + lon + "&units=metric&appid=117dc4bba31cb1ddc1ceeb399015666e";
+
+        fetch(weatherURI)
+        .then(function(weatherResponse){
+            return weatherResponse.json();
+        })
+        .then(function(weatherJson){
+
+            const {name} = weatherJson;
+            const {temp} = weatherJson.main;
+
+
+            let weatherObject = {
+                cityName: name, 
+                temperature: temp
+            }
+
+            processWeatherResponse(weatherObject);
+        })
+    })
 }
