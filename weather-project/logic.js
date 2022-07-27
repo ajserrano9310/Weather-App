9f86e007d0bf14fa77148593c1cd8ea773bbf962
Alejandro <50674421+ajserrano9310@users.noreply.github.com>
2022-07-26 19:26:35 -0600

    Finished tiny weather app

--- weather-project/logic.js
+++ weather-project/logic.js
@@ -3,49 +3,63 @@ var uri = "https://api.openweathermap.org/data/2.5/weather?lat=40.7608&lon=-111.
 
 const API_KEY = "117dc4bba31cb1ddc1ceeb399015666e";
 const DEFAULT_CITY = "New York";
+const IMAGES = ["url('/resources/big-city.jpg')", "url('/resources/wp2752752.jpg')"]
+const CITIES = ["Salt Lake City", "Chicago", "New York", "Seattle"];
+const IMAGE_INTERVAL = 10; 
+
+var index = 0;
+var cityIndex = 0;
+var timeRemaining = 10;
+var counter = 1;
+var counterToChangeCities = 120;
+
+var counterDisplay = document.getElementById("counterDisplay");
+
+const timeToChangeCity = 20; 
 
 window.onload = (event) => {
     let cityUri = "http://api.openweathermap.org/geo/1.0/direct?q=" + DEFAULT_CITY + "&limit=5&appid=" + API_KEY;
     processUserRequest(cityUri);
+    countDown();
 }
 
-const getInformation = function(){
+const getInformation = function () {
 
     fetch(uri)
-    .then((response) => response.json())
-    .then((data) => console.log(data));         
+        .then((response) => response.json())
+        .then((data) => console.log(data));
 }
 
-function getInformation2(){
+function getInformation2() {
     fetch(uri)
-    // Gets Api call
-    .then(
-        function(response){
-            return response.json();
-        }
-        // Get the json object with the information
-    ).then(function (jsonObject){
+        // Gets Api call
+        .then(
+            function (response) {
+                return response.json();
+            }
+            // Get the json object with the information
+        ).then(function (jsonObject) {
 
-        console.log(jsonObject);
-        
-        const {name} = jsonObject;
-        const {temp} = jsonObject.main;
+            console.log(jsonObject);
 
-        let weatherObject = {
-            cityName: name, 
-            temperature: temp
-        }
+            const { name } = jsonObject;
+            const { temp } = jsonObject.main;
 
-        processWeatherResponse(weatherObject);
+            let weatherObject = {
+                cityName: name,
+                temperature: temp
+            }
 
-        
-    })    
+            processWeatherResponse(weatherObject);
+
+
+        })
 }
 
-function processWeatherResponse(obj){
+function processWeatherResponse(obj) {
 
     let cityText = document.getElementsByClassName("city-wrapper")[0];
-    cityText.innerHTML = obj.cityName; 
+    cityText.innerHTML = obj.cityName;
 
     let weatherText = document.getElementsByClassName("temperature")[0];
     weatherText.innerHTML = obj.temperature;
@@ -54,45 +68,81 @@ function processWeatherResponse(obj){
 
 }
 
-function processUserInput(){
+function processUserInput() {
 
-    let city = document.getElementById("cityName").value; 
-    let cityUri = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=5&appid="+API_KEY;
+    let city = document.getElementById("cityName").value;
+    let cityUri = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=5&appid=" + API_KEY;
     processUserRequest(cityUri);
     document.getElementById("cityName").value = "";
-    
+
 }
 
-function processUserRequest(cityUri){
+function processUserRequest(cityUri) {
 
     fetch(cityUri)
-    .then(function(response){
-        return response.json();
+        .then(function (response) {
+            return response.json();
 
-    }).then(function(responseJson){
+        }).then(function (responseJson) {
 
-        const {lat, lon} = responseJson[0];
-        console.log(responseJson);
+            const { lat, lon } = responseJson[0];
+            console.log(responseJson);
 
-        let weatherURI = "https://api.openweathermap.org/data/2.5/weather?lat=" +lat + "&" + "lon=" + lon + "&units=metric&appid=117dc4bba31cb1ddc1ceeb399015666e";
+            let weatherURI = "https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&" + "lon=" + lon + "&units=metric&appid=117dc4bba31cb1ddc1ceeb399015666e";
 
-        fetch(weatherURI)
-        .then(function(weatherResponse){
-            return weatherResponse.json();
+            fetch(weatherURI)
+                .then(function (weatherResponse) {
+                    return weatherResponse.json();
+                })
+                .then(function (weatherJson) {
+                    console.log(weatherJson);
+                    const { name } = weatherJson;
+                    const { temp } = weatherJson.main;
+                    const { main } = weatherJson.weather[0];
+
+                    let weatherObject = {
+                        cityName: name,
+                        temperature: temp,
+                        status: main
+                    }
+
+                    processWeatherResponse(weatherObject);
+                })
         })
-        .then(function(weatherJson){
-            console.log(weatherJson);
-            const {name} = weatherJson;
-            const {temp} = weatherJson.main;
-            const {main} = weatherJson.weather[0];
+}
 
-            let weatherObject = {
-                cityName: name, 
-                temperature: temp,
-                status: main
-            }
+function changeImages() {
 
-            processWeatherResponse(weatherObject);
-        })
-    })
+    if(index === IMAGES.length){
+        index = 0;
+    }
+    document.body.style.backgroundImage = IMAGES[index];
+    index++;
+}
+
+const countDown = setInterval(function () {
+
+    counter++;
+
+    if (counter >= timeRemaining) {
+        changeImages();
+        resetTimer();
+    }
+
+}, 1000);
+
+function resetTimer(){
+
+    counterToChangeCities += counter;
+    if(counterToChangeCities === timeToChangeCity){   
+        let cityUri = "http://api.openweathermap.org/geo/1.0/direct?q=" + CITIES[cityIndex] + "&limit=5&appid=" + API_KEY;
+        processUserRequest(cityUri);
+        counterToChangeCities = 0;
+
+        if(cityIndex === CITIES.length){
+            cityIndex = 0;
+        }
+        cityIndex++;
+    }
+    counter = 0;
 }
